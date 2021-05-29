334e0695c2877ef3e6211ca721d7ee1dc2ec77e0
kafle1 <kafleniraj@gmail.com>
2021-05-29 21:40:28 +0545

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+Project Name: Keeper App
+Technology Used: HTML5, CSS3, JavaScript(Vanilla, React)
+Description: Keeper app is a note keeping app that helps you to take your notes and pin it in the site.
+Developer: Niraj Kafle
+Contact: kafleniraj@gmail.com
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+  const [animation, setAnimation] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+  function handleAnimation(event) {
+    setAnimation(true);
+  }
+  return (
+    <div>
+      <form className="create-note">
+        {animation ? (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        ) : null}
+
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={animation ? "3" : "1"}
+          onClick={handleAnimation}
+        />
+        <Zoom in={animation}>
+          <Fab onClick={submitNote}>
+            <AddIcon />{" "}
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
